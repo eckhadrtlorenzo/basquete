12351030054f6da33925539b97f6c259c9000c42
eckhadrtlorenzo <lorenzo.anzoategui@escola.pr.gov.br>
2025-08-14 08:47:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+Selecione()
+let ingredientes = [
+    { item: "cocolate_branco", quantidade:"10", preço:"R$ 200" };
+    { item: "cocolate_branco", quantidade:"10", preço:"R$ 200" };
+    { item: "cocolate_branco", quantidade:"10", preço:"R$ 200" };
+]
